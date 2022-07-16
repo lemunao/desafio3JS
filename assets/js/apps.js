374bbfd4a5337c5c80495160ab48f3970c822ab9
lemunao <pedroandres1@gmail.com>
2022-07-16 11:04:56 -0400

    color changes done

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,22 +1,26 @@
 document.addEventListener('keydown', (event) => {
     if (event.key === 'a') {
-        /* Cambiar a color 1 */
+        /* Color a change */
         ele = document.querySelector("#div1")
         ele.style.backgroundColor = 'purple'
         return;
     } else if (event.key === 's') {
+        //Color s change
         ele = document.querySelector("#div2")
         ele.style.backgroundColor = 'orange'
         return;
     } else if (event.key === 'd') {
+        //Color d change
         ele = document.querySelector("#div3")
         ele.style.backgroundColor = 'skyblue'
         return;
     } else if(event.key === 'f'){
+        //color f change
         ele = document.querySelector("#div4")
         ele.style.backgroundColor = 'magenta'
         return;
     } else{
+        //Reset HTML, any other key
         location.reload()
     }
 })
